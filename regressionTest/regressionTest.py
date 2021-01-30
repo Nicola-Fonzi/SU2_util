e8b9073dc8f921171df9df6e6d9076fd9f04c275
Nicola-Fonzi <nicola.fonzi@polimi.it>
2021-01-30 14:28:29 +0100

    Introduced better option parser

--- regressionTest/regressionTest.py
+++ regressionTest/regressionTest.py
@@ -1,31 +1,58 @@
 import os
-from optparse import OptionParser
+import argparse
 
 def main():
 
-    parser=OptionParser()
-    parser.add_option("--clean", action="store_true",
-                      help="Specify if we only want to clean the directories", dest="clean", default=False)
-    (options, args)=parser.parse_args()
+    parser = argparse.ArgumentParser()
 
-    HOME = "/scratch/aero/nfonzi/SU2_util/regressionTest/2D-NACA0012/RANS"
+    parser.add_argument("-c","--clean", action="store_true",
+                      help="Specify if we only want to clean the directories", dest="clean", default=False)
+
+    parser.add_argument("-s","--short", action="store_true",
+                      help="Specify if we only want to perform few steps for verification", dest="short", default=False)
+
+    parser.add_argument("-r", "--required",
+                      dest="tests",
+                      choices=["aeroOnly", "static", "forced","dynamicRans","dynamicEuler"],
+                      default=["aeroOnly", "static", "forced","dynamicRans","dynamicEuler"],
+                      nargs="+",
+                      help='Regression tests required',)
+
+    args=parser.parse_args()
+
+    HOME = "/scratch/aero/nfonzi/SU2_util/regressionTest/2D-NACA0012"
     testList = []
 
-    testList.append("/aeroOnly/pureSU2")
-    testList.append("/aeroOnly/interface")
-    testList.append("/static_aeroelasticity")
-    testList.append("/forced_sine")
-    testList.append("/dynamic_aeroelasticity/Ma01")
-    testList.append("/dynamic_aeroelasticity/Ma02")
-    testList.append("/dynamic_aeroelasticity/Ma03")
-    testList.append("/dynamic_aeroelasticity/Ma0357")
-    testList.append("/dynamic_aeroelasticity/Ma0364")
-    testList.append("/dynamic_aeroelasticity/Ma02DiffRS")
+    if "aeroOnly" in args.tests:
+        testList.append("/RANS/aeroOnly/pureSU2")
+        testList.append("/RANS/aeroOnly/interface")
+    if "static" in args.tests:
+        testList.append("/RANS/static_aeroelasticity")
+    if "forced" in args.tests:
+        testList.append("/RANS/forced_sine")
+    if "dynamicRans" in args.tests:
+        testList.append("/RANS/dynamic_aeroelasticity/Ma01")
+        testList.append("/RANS/dynamic_aeroelasticity/Ma02")
+        testList.append("/RANS/dynamic_aeroelasticity/Ma03")
+        testList.append("/RANS/dynamic_aeroelasticity/Ma0357")
+        testList.append("/RANS/dynamic_aeroelasticity/Ma0364")
+        testList.append("/RANS/dynamic_aeroelasticity/Ma02DiffRS")
+    if "dynamicEuler" in args.tests:
+        testList.append("/Euler/dynamic_aeroelasticity/Ma01")
+        testList.append("/Euler/dynamic_aeroelasticity/Ma02")
+        testList.append("/Euler/dynamic_aeroelasticity/Ma03")
+        testList.append("/Euler/dynamic_aeroelasticity/Ma0357")
+        testList.append("/Euler/dynamic_aeroelasticity/Ma0364")
 
     for test in testList:
         os.chdir(HOME+test)
-        if options.clean:
+        if args.clean:
             os.system("rm *vtu FSI* Struct* log* histo*")
+        elif args.short:
+            if os.path.isfile("fsi_short.cfg"):
+                os.system("mpirun -np 38 python3 /scratch/aero/nfonzi/SU2/bin/fsi_computation.py --parallel -f fsi_short.cfg > log.txt")
+            else:
+                os.system("mpirun -np 38 SU2_CFD fluid_short.cfg > log.txt")
         else:
             if os.path.isfile("fsi.cfg"):
                 os.system("mpirun -np 38 python3 /scratch/aero/nfonzi/SU2/bin/fsi_computation.py --parallel -f fsi.cfg > log.txt")
